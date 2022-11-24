4a9e84e2bde05645cda1de42049f660ad065f4bc
sohan653 <sohanurl653@gmail.com>
2022-11-24 22:27:25 +0600

    cors added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/folder');
+const cors=require('cors')
 require('dotenv').config()
 
 const app=express();
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(process.env.URL).then(()=>{console.log("connected mongodb")})
 
